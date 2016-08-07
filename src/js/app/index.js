ef73572302994ddbc9f8c38d76a651e97b84df57
marshallford <inbox@marshallford.me>
2016-08-07 17:34:20 -0500

    RHL and router working

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { Router } from 'react-router'
+import makeRoutes from 'meta/routes'
 
-const App = ({ store, history, routes }) => (
+const routes = makeRoutes()
+
+const App = ({ store, history }) => (
   <Provider store={store}>
     <Router history={history} routes={routes} />
   </Provider>
--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import { AppContainer as HotLoader } from 'react-hot-loader'
 import App from 'app'
-import makeRoutes from 'meta/routes'
 import configureStore from 'meta/store/configureStore'
 import { browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
@@ -11,31 +10,29 @@ import { syncHistoryWithStore } from 'react-router-redux'
 const store = configureStore(browserHistory)
 window.store = store
 const history = syncHistoryWithStore(browserHistory, store)
-const routes = makeRoutes(store)
+
+const el = document.getElementById('app')
 
 render(
   <HotLoader>
     <App
       store={store}
       history={history}
-      routes={routes}
     />
   </HotLoader>,
-  document.getElementById('app')
+  el
 )
 
 if (module.hot) {
   module.hot.accept('app', () => {
-    const AppNext = require('app').default
     render(
       <HotLoader>
-        <AppNext
+        <App
           store={store}
           history={history}
-          routes={routes}
         />
       </HotLoader>,
-      document.getElementById('app')
+      el
     )
   })
 }
