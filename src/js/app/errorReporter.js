8a48f15e9baa502946d4356f514c7a26b29dabb1
marshallford <inbox@marshallford.me>
2016-08-31 11:17:59 -0500

    adds error stack to errorReporter

--- src/js/app/errorReporter.js
+++ src/js/app/errorReporter.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Redbox from 'redbox-react'
 
+// https://github.com/gaearon/react-hot-loader/issues/312#issuecomment-237813284
 const errorReporter = ({ error }) => {
-  console.error(error)
+  if (error) {
+    console.error(error.message, error.stack)
+  }
   return <Redbox error={error} />
 }
 
